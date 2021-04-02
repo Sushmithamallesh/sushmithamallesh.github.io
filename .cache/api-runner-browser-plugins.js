9ee1a5b8a1f83190630b2c13aad4b5920e83f860
Sushmithamallesh <sushdec6@gmail.com>
2021-04-02 22:24:50 +0530

    REmove node modules

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"Sergei Chestakov - Portfolio","short_name":"Sergei","start_url":"/","background_color":"#212121","theme_color":"#212121","display":"minimal-ui","icon":"src/assets/images/favicon.jpg","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"0af07b895ec1dee2abc4b7d5bdf4c5d3"},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
