44b29d76555c3d4b5e89dc2196c2c7fa57615daf
Sushmithamallesh <sushdec6@gmail.com>
2021-09-18 13:15:04 +0530

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-186da249d514a11fbb62.js"
+    "url": "webpack-runtime-9a52fba401c61fdcf458.js"
   },
   {
     "url": "framework-d585a04b7d285a7760d6.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ff87848a28a29e458f517c41483b10b6"
+    "revision": "d3f49f464ad827e28c36a7fa6e555b96"
   },
   {
     "url": "polyfill-e4ff66ed5ec0b4bd2598.js"
